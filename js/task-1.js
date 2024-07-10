f650def89dc56e83d95da3540bcb6dd637d54120
gizemcoban <gizem.coban.r@gmail.com>
2024-07-10 23:30:17 +0300

    functions are done

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+//Görev 1. slug oluşturucu
+console.log("-----slugify-----");
+function slugify(title){
+    newTitle = title.split(" ").join("-").toLowerCase();
+    return newTitle;
+}
+
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
